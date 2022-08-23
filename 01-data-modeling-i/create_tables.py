a2dda6c1e60a278070ef96a859fbd1e67f06a768
chin-lertvipada <chin.lertvipada@g.swu.ac.th>
2022-08-23 12:05:28 +0000

    data modeling

--- 01-data-modeling-i/create_tables.py
+++ 01-data-modeling-i/create_tables.py
@@ -1,19 +1,58 @@
 import psycopg2
 
 
-table_drop = "DROP TABLE IF EXISTS songplays"
+table_drop_repo  = "DROP TABLE IF EXISTS Repo;"
+table_drop_org   = "DROP TABLE IF EXISTS Org;"
+table_drop_actor = "DROP TABLE IF EXISTS Actor;"
+table_drop_event = "DROP TABLE IF EXISTS Event;"
 
-table_create = """
-    CREATE TABLE IF NOT EXISTS xxx (
-    )
+table_create_repo = """
+    CREATE TABLE IF NOT EXISTS Repo (
+        repo_id BIGINT NOT NULL,
+        repo_name VARCHAR(100) NOT NULL,
+        repo_url VARCHAR(150) NOT NULL,
+        PRIMARY KEY (repo_id)
+    );
+"""
+table_create_org = """
+    CREATE TABLE IF NOT EXISTS Repo (
+        org_id BIGINT NOT NULL,
+        org_login VARCHAR(50) NOT NULL,
+        org_gravatar_id VARCHAR(50),
+        org_url VARCHAR(100) NOT NULL,
+        org_avatar_url VARCHAR(100) NOT NULL,
+        PRIMARY KEY (org_id)
+    );
+"""
+table_create_actor = """
+    CREATE TABLE IF NOT EXISTS Repo (
+        actor_id BIGINT NOT NULL,
+        actor_login VARCHAR(50) NOT NULL,
+        actor_display_login VARCHAR(50) NOT NULL,
+        actor_gravatar_id VARCHAR(50),
+        actor_url VARCHAR(100) NOT NULL,
+        actor_avatar_url VARCHAR(100) NOT NULL,
+        PRIMARY KEY (actor_id)
+    );
+"""
+table_create_event = """
+    CREATE TABLE IF NOT EXISTS Repo (
+        event_id VARCHAR(20) NOT NULL,
+        event_type VARCHAR(50) NOT NULL,
+        event_public BOOLEAN NOT NULL,
+        event_created_at TIMESTAMP NOT NULL,
+        event_repo_id BIGINT NOT NULL,
+        event_actor_id BIGINT NOT NULL,
+        event_org_id BIGINT,
+        PRIMARY KEY (event_id),
+        FOREIGN KEY (event_repo_id)  REFERENCES Repo  (repo_id),
+        FOREIGN KEY (event_actor_id) REFERENCES Actor (actor_id),
+        FOREIGN KEY (event_org_id)   REFERENCES Org   (org_id)
+    );
 """
 
-create_table_queries = [
-    table_create,
-]
-drop_table_queries = [
-    table_drop,
-]
+create_table_queries = [table_create_repo, table_create_org, table_create_actor, table_create_event]
+drop_table_queries   = [table_drop_repo, table_drop_org, table_drop_actor, table_drop_event]
 
 
 def drop_tables(cur: PostgresCursor, conn: PostgresConn) -> None:
---
+++ 01-data-modeling-i/script_create.sql
@@ -0,0 +1,42 @@
+CREATE TABLE public.Repo (
+                repo_id BIGINT NOT NULL,
+                repo_name VARCHAR(100) NOT NULL,
+                repo_url VARCHAR(150) NOT NULL,
+                PRIMARY KEY (repo_id)
+);
+
+
+CREATE TABLE public.Org (
+                org_id BIGINT NOT NULL,
+                org_login VARCHAR(50) NOT NULL,
+                org_gravatar_id VARCHAR(50),
+                org_url VARCHAR(100) NOT NULL,
+                org_avatar_url VARCHAR(100) NOT NULL,
+                PRIMARY KEY (org_id)
+);
+
+
+CREATE TABLE public.Actor (
+                actor_id BIGINT NOT NULL,
+                actor_login VARCHAR(50) NOT NULL,
+                actor_display_login VARCHAR(50) NOT NULL,
+                actor_gravatar_id VARCHAR(50),
+                actor_url VARCHAR(100) NOT NULL,
+                actor_avatar_url VARCHAR(100) NOT NULL,
+                PRIMARY KEY (actor_id)
+);
+
+
+CREATE TABLE public.Event (
+                event_id VARCHAR(20) NOT NULL,
+                event_repo_id BIGINT NOT NULL,
+                event_actor_id BIGINT NOT NULL,
+                event_org_id BIGINT,
+                event_type VARCHAR(50) NOT NULL,
+                event_public BOOLEAN NOT NULL,
+                event_created_at TIMESTAMP NOT NULL,
+                PRIMARY KEY (event_id),
+                FOREIGN KEY (event_repo_id)  REFERENCES Repo  (repo_id),
+                FOREIGN KEY (event_actor_id) REFERENCES Actor (actor_id),
+                FOREIGN KEY (event_org_id)   REFERENCES Org   (org_id)
+);
