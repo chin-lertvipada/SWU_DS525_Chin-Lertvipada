de19364ec0b2992aba1ed9a52cd1dd21d44d7c0f
chin-lertvipada <chin.lertvipada@g.swu.ac.th>
2022-09-13 21:52:45 +0700

    code final

--- 03-building-a-data-warehouse/etl.py
+++ 03-building-a-data-warehouse/etl.py
@@ -2,9 +2,10 @@ import psycopg2
 
 
 drop_table_queries = [
+    "DROP TABLE IF EXISTS staging_events",
     "DROP TABLE IF EXISTS events",
-    # "DROP TABLE IF EXISTS actors",
-    # "DROP TABLE IF EXISTS repos",
+    "DROP TABLE IF EXISTS actors",
+    "DROP TABLE IF EXISTS repos",
 ]
 
 create_table_queries = [
@@ -12,89 +13,69 @@ create_table_queries = [
     CREATE TABLE IF NOT EXISTS staging_events (
         id text,
         type text,
-        actor text,
-        repo text,
-        created_at timestamp
+        actor_id bigint,
+        actor_name text,
+        actor_url text,
+        repo_id bigint,
+        repo_name text,
+        repo_url text,
+        public boolean,
+        created_at text
     )
     """,
     """
     CREATE TABLE IF NOT EXISTS events (
-        id text
+        id text,
+        type text,
+        actor text,
+        repo text,
+        created_at text
     )
     """,
-    # """
-    # CREATE TABLE IF NOT EXISTS staging_events (
-    #     id text,
-    #     type text,
-    #     actor_id bigint,
-    #     actor_name text,
-    #     actor_url text,
-    #     repo_id bigint,
-    #     repo_name text,
-    #     repo_url text,
-    #     public boolean,
-    #     created_at timestamp
-    # )
-    # """,
-    # """
-    # CREATE TABLE IF NOT EXISTS events (
-    #     id text,
-    #     type text,
-    #     actor text,
-    #     repo text,
-    #     created_at timestamp
-    # )
-    # """,
-    # """
-    # CREATE TABLE IF NOT EXISTS actors (
-    #     id bigint,
-    #     name text,
-    #     url text
-    # )
-    # """,
-    # """
-    # CREATE TABLE IF NOT EXISTS repos (
-    #     id bigint,
-    #     name text,
-    #     url text
-    # )
-    # """,
+    """
+    CREATE TABLE IF NOT EXISTS actors (
+        id bigint,
+        name text,
+        url text
+    )
+    """,
+    """
+    CREATE TABLE IF NOT EXISTS repos (
+        id bigint,
+        name text,
+        url text
+    )
+    """,
 ]
 
 copy_table_queries = [
     """
-    COPY staging_events FROM 's3://chin-swu-lab/github_events_01.json'
+    COPY staging_events FROM 's3://chin-swu-lab3/github_events_01.json'
     CREDENTIALS 'aws_iam_role=arn:aws:iam::535584994987:role/LabRole'
-    JSON 's3://chin-swu-lab/events_json_path.json'
+    JSON 's3://chin-swu-lab3/events_json_path.json'
     REGION 'us-east-1'
     """,
 ]
 
 insert_table_queries = [
     """
-    INSERT INTO events ( id )
-    SELECT DISTINCT id 
+    INSERT INTO events ( id, type, actor, repo, created_at )
+    SELECT DISTINCT id, type, actor_name, repo_name, created_at
     FROM staging_events
     WHERE id NOT IN (SELECT DISTINCT id FROM events)
     """,
-    # """
-    # INSERT INTO events ( id, type, actor, repo, created_at )
-    # SELECT DISTINCT id, type, actor_name, repo_name, created_at
-    # FROM staging_events
-    # WHERE id NOT IN (SELECT DISTINCT id FROM events)
-    # """,
-    # """
-    # INSERT INTO actors ( id, name, url )
-    # SELECT DISTINCT actor_id, actor_name, actor_url
-    # FROM staging_events
-    # WHERE actor_id NOT IN (SELECT DISTINCT id FROM actors)
-    # """,
-    # """
-    # INSERT INTO repos ( id, name, url )
-    # SELECT DISTINCT repo_id, repo_name, repo_url
-    # FROM staging_events
-    # WHERE id NOT IN (SELECT DISTINCT id FROM repos)
-    # """,
+    """
+    INSERT INTO actors ( id, name, url )
+    SELECT DISTINCT actor_id, actor_name, actor_url
+    FROM staging_events
+    WHERE actor_id NOT IN (SELECT DISTINCT id FROM actors)
+    """,
+    """
+    INSERT INTO repos ( id, name, url )
+    SELECT DISTINCT repo_id, repo_name, repo_url
+    FROM staging_events
+    WHERE id NOT IN (SELECT DISTINCT id FROM repos)
+    """,
 ]
 
 
@@ -134,15 +115,6 @@ def main():
 
     drop_tables(cur, conn)
     create_tables(cur, conn)
-
-    # query data
-    query = "select * from events"
-    cur.execute(query)
-    # print data
-    records = cur.fetchall()
-    for row in records:
-        print(row)
-
     load_staging_tables(cur, conn)
     insert_tables(cur, conn)
 
